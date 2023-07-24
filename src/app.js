8ce63fa0442de867030a168d5015fb2dacef753e
Rafael <rafael_fermino_10@hotmail.com>
2023-07-23 21:45:42 -0300

    Implementação Banco

--- src/app.js
+++ src/app.js
@@ -7,15 +7,24 @@
 import express from "express";
 
 const app  = express();
+app.use(express.json());    
 
 const categoria = [
     {"nome": "Rafael",telefone: 1144218098,telefone_1 : 11995738799, id: 1},
-    {"nome": "Rafael",telefone: 1144218098,telefone_1 : 11995738799, id: 1}   
+    {"nome": "Rafael",telefone: 1144218098,telefone_1 : 11995738799, id: 2}   
 ]
 
 app.get('/', (req, res) => {
     res.status(200);
     res.json(categoria);
 });
+app.post('/', (req, res) => {
+    categoria.push(req.body);
+    res.status(201).send('Welcome to the database');
+    
+});
+app.put('/:id', (req, res) => {
+});
+app.delete ('/:id', (req, res) => {});
 
 export default app
---
+++ src/config/dbConnect.js
@@ -0,0 +1,3 @@
+import moongose from "moongose";
+
+moongose.connect("mongodb+srv://rlacerdafermino:123@cluster0.d05ya0h.mongodb.net/")
